4ac7ab9c622ac864e8743d63a18fe4402d6e7258
Francisco barrios <frankybarrios1@icloud.com>
2020-08-19 21:59:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
 # lib/introduction.rb
-def introduction(Francisco)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 # lib/introduction_with_language.rb
-  def introduction_with_language(Francisco, Java)
-    puts "Hi, my name is #{name} and i am learning to program in #{language}."
+  def introduction_with_language(name, language)
+    puts "Hi, my name is #{name}. I am learning to program in #{language}."
 end
